91a004aa7cff24384387a55e5aac014020cca377
Marcelo91 <marcelocotia91@gmail.com>
2023-08-23 08:10:07 -0300

    adicionando codigo forca.py

---
+++ forca.py
@@ -0,0 +1,13 @@
+
+def jogar():
+
+    print("************************************")
+    print("**** Bem vindo ao jogo da forca ****")
+    print("************************************")
+
+
+
+    print('Fim de jogo!!!')
+
+if(__name__ == '__main__'):
+    jogar()
