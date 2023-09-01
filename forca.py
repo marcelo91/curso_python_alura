c18ad7ff2b85c73dcf5ec06f0e58d0c7b2c3b2f6
Marcelo91 <marcelocotia91@gmail.com>
2023-08-31 21:35:15 -0300

    Atualizando codigo forca

--- forca.py
+++ forca.py
@@ -1,13 +1,20 @@
-
 def jogar():
-
     print("************************************")
-    print("**** Bem vindo ao jogo da forca ****")
+    print("****Bem vindo ao jogo da forca!****")
     print("************************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual letra você escolhe?")
+
+        print("Jogando!!!")
+
+    print("Fim de jogo!!!")
 
 
-    print('Fim de jogo!!!')
-
-if(__name__ == '__main__'):
+if __name__ == "__main__":
     jogar()
