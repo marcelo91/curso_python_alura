8e7291e377d78f4d25bca26acbfd1ca2fbccd139
Marcelo91 <marcelocotia91@gmail.com>
2023-09-01 08:48:00 -0300

    Atualizando forca.py e Anotações

--- forca.py
+++ forca.py
@@ -3,16 +3,45 @@ def jogar():
     print("****Bem vindo ao jogo da forca!****")
     print("************************************")
 
-    palavra_secreta = "banana"
+    # minha palavra secreta, e (upper)deixando maiusculo
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
+    # enquanto variaveis (enforcou e acertou) são falsas faça:
     while not enforcou and not acertou:
+        # pegando palavra dada pelo usuário
         chute = input("Qual letra você escolhe?")
+        # (strip)tirando os espaços da palavra caso existam e (upper)deixando maiusculo
+        chute = chute.strip().upper()
+        # definindo um index para que possa mostrar a posição da letra dentro da palavra
+        index = 0
+
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                # condição de acerto e transformação em maiusculo
+                if chute == letra:
+                    # inserir a letra mencionada na posição correta da palavra
+                    letras_acertadas[index] = chute
+                index += 1
+        else:
+            erros += 1
+
+        print(letras_acertadas)
+
+        #logicas para saida do while
+        enforcou = erros == 6
+        acertou = ("_") not in letras_acertadas
 
         print("Jogando!!!")
 
+    if acertou:
+        print("Ganhou!!!")
+    else:
+        print("Perdeu!!!")
     print("Fim de jogo!!!")
 
 
